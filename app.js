a23a4bc88386c314a9e849661481b7de3615eedf
MayurBhowad <mayurbhowad1995@gmail.com>
2021-03-26 19:49:25 +0530

    edit: css-responsiveness

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ connectDB();
 
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://raftlabs.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
